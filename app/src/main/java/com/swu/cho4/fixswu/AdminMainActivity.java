0ad1801024332c98210aaf83024928df90830ae0
ParkSeoEun <silion10@naver.com>
2019-07-15 15:45:16 +0900

    Merge branch 'master' of C:\GURU2\FixSWU with conflicts.

--- app/src/main/java/com/swu/cho4/fixswu/AdminMainActivity.java
+++ app/src/main/java/com/swu/cho4/fixswu/AdminMainActivity.java
@@ -3,12 +3,19 @@ package com.swu.cho4.fixswu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AdminMainActivity extends AppCompatActivity {
+
+    private TextView hearNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+
+        hearNum = findViewById(R.id.heartNum);
+        //TODO 하트 수 표시
+        //hearNum.setText();
     }
 }
