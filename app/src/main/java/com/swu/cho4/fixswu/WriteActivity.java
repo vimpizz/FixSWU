3da36ec34ad58dc4e5407ec76838d390740d31f7
ParkSeoEun <silion10@naver.com>
2019-07-15 16:11:29 +0900

    MainActivity

--- app/src/main/java/com/swu/cho4/fixswu/WriteActivity.java
+++ app/src/main/java/com/swu/cho4/fixswu/WriteActivity.java
@@ -1,5 +1,15 @@
 package com.swu.cho4.fixswu;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -11,6 +21,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
