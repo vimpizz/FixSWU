20192821814fe13c5b71690c721932719ed21601
vimpizz <vimpizz321@gmail.com>
2019-07-15 11:41:13 +0900

    엑티비티

---
+++ app/src/main/java/com/swu/cho4/fixswu/BoardBean.java
@@ -0,0 +1,18 @@
+package com.swu.cho4.fixswu;
+
+import android.graphics.Bitmap;
+
+public class BoardBean {
+    public String stuNum;
+    public String name;
+    public String house;
+    public String roomNum;
+    public String deskNum;
+    public String imgUrl;
+    public String content;
+    public String date;
+    public String comment;
+    public boolean like;
+    public transient Bitmap bmpTitle;
+
+}
