3598a22a6edd3cb35f792535c4a08eea459294a2
vimpizz <vimpizz321@gmail.com>
2019-07-15 15:36:24 +0900

    write Activity 초안

--- app/src/main/java/com/swu/cho4/fixswu/BoardBean.java
+++ app/src/main/java/com/swu/cho4/fixswu/BoardBean.java
@@ -3,12 +3,18 @@ package com.swu.cho4.fixswu;
 import android.graphics.Bitmap;
 
 public class BoardBean {
+    public String id;
+    public String userId;
+
     public String stuNum;
     public String name;
-    public String house;
+    public int house;
     public String roomNum;
     public String deskNum;
-    public String imgUrl;
+
+    public String imgUri;
+    public String imgName;
+
     public String content;
     public String date;
     public String comment;
