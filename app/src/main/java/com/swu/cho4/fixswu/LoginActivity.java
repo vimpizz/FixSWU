9c1fba67c655ac7ff2153176e9d795691e93378f
vimpizz <vimpizz321@gmail.com>
2019-07-16 14:56:23 +0900

    modify,detail activity 수정

--- app/src/main/java/com/swu/cho4/fixswu/LoginActivity.java
+++ app/src/main/java/com/swu/cho4/fixswu/LoginActivity.java
@@ -59,7 +59,7 @@ public class LoginActivity extends AppCompatActivity {
                         ,Toast.LENGTH_SHORT).show();
                 goAdminMainActivity();
             } else {
-                Toast.makeText(getBaseContext(), "Main"
+                Toast.makeText(getBaseContext(), "Loading..."
                         , Toast.LENGTH_SHORT).show();
                 goMainActivity();
             }
@@ -111,8 +111,7 @@ public class LoginActivity extends AppCompatActivity {
                                         ,Toast.LENGTH_SHORT).show();
                                 goAdminMainActivity();
                             } else {
-                                Toast.makeText(getBaseContext(), "Loading"
-                                        , Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getBaseContext(), "Loading...", Toast.LENGTH_SHORT).show();
                                 goMainActivity();
                             }
                         } else {
