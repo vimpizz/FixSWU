57cb542a2c9385d1c8e30d42014cbda500d59762
vimpizz <vimpizz321@gmail.com>
2019-07-16 12:02:09 +0900

    modify,detail activity 추가

---
+++ app/src/main/java/com/swu/cho4/fixswu/DetailBoardActivity.java
@@ -0,0 +1,17 @@
+package com.swu.cho4.fixswu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_board);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/swu/cho4/fixswu/ModifyBoardActivity.java
@@ -0,0 +1,15 @@
+package com.swu.cho4.fixswu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifyBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_board);
+
+    }
+}
