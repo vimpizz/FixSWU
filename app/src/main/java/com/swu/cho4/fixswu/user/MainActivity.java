66ca4c9d3df36d3a95c7962caa77cfe58fa2d836
thanksthanks <flzl0308@naver.com>
2019-07-19 13:58:18 +0900

    Merge branch 'master' of C:\Users\PC\AndroidStudioProjects\FixSWU with conflicts.

--- app/src/main/java/com/swu/cho4/fixswu/user/MainActivity.java
+++ app/src/main/java/com/swu/cho4/fixswu/user/MainActivity.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.swu.cho4.fixswu.LoginActivity;
+import com.swu.cho4.fixswu.Pop;
 import com.swu.cho4.fixswu.R;
 import com.swu.cho4.fixswu.UserInfoActivity;
 import com.swu.cho4.fixswu.bean.BoardBean;
@@ -56,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         // 회원정보 상세보기 버튼, 추가 버튼
         findViewById(R.id.btnUserInfo).setOnClickListener(mBtnClick);
         findViewById(R.id.btnWrite).setOnClickListener(mBtnClick);
+
 
         // 최초 데이터 세팅
         mBoardAdapter = new BoardAdapter(this, mBoardList);
@@ -104,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
 
     @Override
     public void onBackPressed() {
